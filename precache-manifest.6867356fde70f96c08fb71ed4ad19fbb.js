18734bb421aec47e72c558d17020cab04cd190a6
Stacha Clemens <stacha@192-168-1-107.tpgi.com.au>
2022-08-25 14:52:12 +1000

    Updates

---
+++ precache-manifest.6867356fde70f96c08fb71ed4ad19fbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "133dfa5f28abbbea47d398f91ea5dc8b",
+    "url": "/birthday-reminder/index.html"
+  },
+  {
+    "revision": "4cad3bee860f19dc7769",
+    "url": "/birthday-reminder/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cad3bee860f19dc7769",
+    "url": "/birthday-reminder/static/js/main.c454ef29.chunk.js"
+  },
+  {
+    "revision": "5567b67584bb31e15f4d",
+    "url": "/birthday-reminder/static/js/runtime-main.35ffc63d.js"
+  }
+]);
